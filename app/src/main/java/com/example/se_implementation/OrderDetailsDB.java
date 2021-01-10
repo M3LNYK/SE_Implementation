987f173f97c7a4eb83ae2787a2d5723e52aeb0bd
annaoliinyk <olejnikanuta@gmail.com>
2021-01-10 21:28:10 +0100

    Added file with database for order details (v.1)

---
+++ app/src/main/java/com/example/se_implementation/OrderDetailsDB.java
@@ -0,0 +1,99 @@
+package com.example.se_implementation;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class OrderDetailsDB extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "order_details_database";
+    public static final String ORDER_DETAILS_TABLE_NAME = "order_detail";
+    public static final String PART_COLUMN_ID = "id";
+    public static final String PART_COLUMN_NAME = "part";
+    public static final String PART_COLUMN_CATEGORY = "category";
+    public static final String PART_COLUMN_PRODUCER = "producer";
+    public static final String PART_COLUMN_NREVIEWS = "number_of_reviews";
+    public static final String DEADLINE_COLUMN_DATE = "deadline";
+    public static final String PERSONAL_NOTE_COLUMN = "personal_note";
+
+    public OrderDetailsDB(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("CREATE TABLE " + ORDER_DETAILS_TABLE_NAME + " (" +
+                PART_COLUMN_ID + " TEXT NOT NULL, " +
+                PART_COLUMN_NAME + " TEXT NOT NULL, " +
+                PART_COLUMN_CATEGORY + " TEXT NOT NULL, " +
+                PART_COLUMN_PRODUCER + " TEXT, " +
+                PART_COLUMN_NREVIEWS + " TEXT, " +
+                DEADLINE_COLUMN_DATE + " TEXT, " +
+                PERSONAL_NOTE_COLUMN + " TEXT NOT NULL, " +
+                "PRIMARY KEY (" + PART_COLUMN_ID + ")," +
+                "FOREIGN KEY (" + PERSONAL_NOTE_COLUMN + ") REFERENCES " + ORDER_DETAILS_TABLE_NAME + " (" + PART_COLUMN_ID + ")" +
+                ")");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + ORDER_DETAILS_TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+
+    boolean addEmployee(String id, String part, String category, String producer, String number_of_reviews, String deadline, String personal_note){
+        SQLiteDatabase sqLiteDatabase = getReadableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(PART_COLUMN_ID, id);
+        contentValues.put(PART_COLUMN_NAME, part);
+        contentValues.put(PART_COLUMN_CATEGORY, category);
+        contentValues.put(PART_COLUMN_PRODUCER, producer);
+        contentValues.put(PART_COLUMN_NREVIEWS, number_of_reviews);
+        contentValues.put(DEADLINE_COLUMN_DATE, deadline);
+        contentValues.put(PERSONAL_NOTE_COLUMN, personal_note);
+
+        return sqLiteDatabase.insert(ORDER_DETAILS_TABLE_NAME, null, contentValues) != -1;
+    }
+
+    Cursor getAllEmployees() {
+        SQLiteDatabase sqLiteDatabase = getReadableDatabase();
+        return sqLiteDatabase.rawQuery("SELECT * FROM " + ORDER_DETAILS_TABLE_NAME, null);
+    }
+
+    Cursor getAllEmployees(String ownerId) {
+        SQLiteDatabase sqLiteDatabase = getReadableDatabase();
+        return sqLiteDatabase.rawQuery("SELECT * FROM " + ORDER_DETAILS_TABLE_NAME
+                + " WHERE " + PERSONAL_NOTE_COLUMN + "=" + ownerId, null);
+    }
+
+    boolean updateEmployee(String id, String part, String category, String producer, String number_of_reviews, String deadline, String personal_note) {
+
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(PART_COLUMN_ID, id);
+        contentValues.put(PART_COLUMN_NAME, part);
+        contentValues.put(PART_COLUMN_CATEGORY, category);
+        contentValues.put(PART_COLUMN_PRODUCER, producer);
+        contentValues.put(PART_COLUMN_NREVIEWS, number_of_reviews);
+        contentValues.put(DEADLINE_COLUMN_DATE, deadline);
+        contentValues.put(PERSONAL_NOTE_COLUMN, personal_note);
+        return sqLiteDatabase.update(ORDER_DETAILS_TABLE_NAME, contentValues,
+                PART_COLUMN_ID + "=?",
+                new String[] { id }) == 1;
+    }
+
+    boolean deleteEmployee(String id) {
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+        return sqLiteDatabase.delete(ORDER_DETAILS_TABLE_NAME,
+                PART_COLUMN_ID + "=?" ,
+                new String[] { id }) == 1;
+    }
+    void deleteAll() {
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + ORDER_DETAILS_TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+}
