4b8c76715eb1fea23bca0eb00a378dd075c1018f
M3LNYK <48330869+M3LNYK@users.noreply.github.com>
2021-01-12 14:20:39 +0200

    Added activity myOrders

--- app/src/main/java/com/example/se_implementation/MainActivity.java
+++ app/src/main/java/com/example/se_implementation/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.se_implementation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button_list_of_orders);
+        button.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), MyOrders.class)));
     }
 }
