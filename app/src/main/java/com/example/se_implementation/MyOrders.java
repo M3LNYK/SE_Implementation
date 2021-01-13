04a7b5a324f64c4e15966646712cf154c10d2c73
M3LNYK <48330869+M3LNYK@users.noreply.github.com>
2021-01-14 01:59:13 +0200

    Created refference to the ViewModel created previously

--- app/src/main/java/com/example/se_implementation/MyOrders.java
+++ app/src/main/java/com/example/se_implementation/MyOrders.java
@@ -1,14 +1,33 @@
 package com.example.se_implementation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MyOrders extends AppCompatActivity {
+    private OrderViewModel orderViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_orders);
+
+        orderViewModel = new ViewModelProvider(this,
+                ViewModelProvider.AndroidViewModelFactory.getInstance(
+                        this.getApplication())).get(OrderViewModel.class);
+
+        orderViewModel.getAllOrders().observe(this, new Observer<List<Order>>() {
+            @Override
+            public void onChanged(List<Order> orders) {
+                //Update RecyclerView
+                Toast.makeText(MyOrders.this,  "onChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
