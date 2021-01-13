2a22af826d4338acb25e770ebd61e54b628b66c2
M3LNYK <48330869+M3LNYK@users.noreply.github.com>
2021-01-13 18:44:03 +0200

    Started creation of filling feature Idea is that when we start our demo, database will be already with something in it. This way it can be a demo in my opinion.

--- app/src/main/java/com/example/se_implementation/OrderDatabase.java
+++ app/src/main/java/com/example/se_implementation/OrderDatabase.java
@@ -1,10 +1,13 @@
 package com.example.se_implementation;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = {Order.class}, version = 1)
 public abstract class OrderDatabase extends RoomDatabase {
@@ -21,5 +24,21 @@ public abstract class OrderDatabase extends RoomDatabase {
                     .build();
         }
         return instance;
+    }
+
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+        }
+    };
+
+    private static class PopulateDBAsyncTask extends AsyncTask<Void, Void, Void> {
+        private OrderDao orderDao;
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            return null;
+        }
     }
 }
