fa9e292e2b43ea3eea0bd2fb7070e1dd90060254
annaoliinyk <olejnikanuta@gmail.com>
2021-01-16 20:03:02 +0100

    Modified parts' database class

--- app/src/main/java/com/example/se_implementation/OrderDetailsPartsDatabase.java
+++ app/src/main/java/com/example/se_implementation/OrderDetailsPartsDatabase.java
@@ -1,6 +1,7 @@
 package com.example.se_implementation;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
 import androidx.annotation.NonNull;
 import androidx.room.Database;
@@ -8,6 +9,7 @@ import androidx.room.DatabaseConfiguration;
 import androidx.room.InvalidationTracker;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 import androidx.sqlite.db.SupportSQLiteOpenHelper;
 
 @Database(entities = {OrderDetailsParts.class}, version =  1)
@@ -21,8 +23,30 @@ public abstract class OrderDetailsPartsDatabase extends RoomDatabase {
         if(instance == null){
              instance = Room.databaseBuilder(context.getApplicationContext(),
                      OrderDetailsPartsDatabase.class, "parts_database")
-                     .fallbackToDestructiveMigration().build();
+                     .fallbackToDestructiveMigration().addCallback(roomCallback).build();
         }
         return instance;
+    }
+
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void>{
+        private OrderDetailsPartsDao orderDetailsPartsDao;
+
+        private PopulateDbAsyncTask(OrderDetailsPartsDatabase db){
+            orderDetailsPartsDao = db.orderDetailsPartsDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            orderDetailsPartsDao.insert(new OrderDetailsParts("detail", "category", "producer"));
+            return null;
+        }
     }
 }
