f453d1142e32bfa4a4a88d11f8d909547bd80179
annaoliinyk <olejnikanuta@gmail.com>
2021-01-17 19:58:07 +0100

    Connected AddPartActivity to its xml file

--- app/src/main/java/com/example/se_implementation/AddPartActivity.java
+++ app/src/main/java/com/example/se_implementation/AddPartActivity.java
@@ -8,5 +8,17 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddPartActivity extends AppCompatActivity {
+    private EditText editTextPartName;
+    private EditText editTextPartCategory;
+    private EditText editTextPartProducer;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_part);
+
+        editTextPartName = findViewById(R.id.nameAdd);
+        editTextPartCategory = findViewById(R.id.categoryAdd);
+        editTextPartProducer = findViewById(R.id.producerAdd);
+    }
 }
