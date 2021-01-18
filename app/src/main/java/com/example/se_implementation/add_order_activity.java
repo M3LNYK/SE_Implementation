2885f58cb38101dca4396a06a65d845a5f6edeb0
M3LNYK <48330869+M3LNYK@users.noreply.github.com>
2021-01-18 15:31:07 +0200

    Test changes added to git

--- app/src/main/java/com/example/se_implementation/add_order_activity.java
+++ app/src/main/java/com/example/se_implementation/add_order_activity.java
@@ -11,7 +11,6 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class add_order_activity extends AppCompatActivity {
-    public static final String EXTR
 
     private EditText editTextCarBrand;
     private EditText editTextCarModel;
