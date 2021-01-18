ae8f51f0dd4a1c93ddc2cc5166ba434153de4008
annaoliinyk <olejnikanuta@gmail.com>
2021-01-18 21:08:44 +0100

    Changed to the version that was 2 days ago

--- app/src/main/java/com/example/se_implementation/DeadlineFragment.java
+++ app/src/main/java/com/example/se_implementation/DeadlineFragment.java
@@ -1,19 +1,15 @@
 package com.example.se_implementation;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Switch;
-import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,21 +17,12 @@ import android.widget.TextView;
  * create an instance of this fragment.
  */
 public class DeadlineFragment extends Fragment {
-    private TextView textView;
-    private EditText editText;
-    private Button applyButton;
-    private Button saveButton;
-    private Switch switch1;
 
-    private static final String SHARED_PREFS = "sharedPrefs";
-    public static final String TEXT = "text";
-    public static final String SWITCH1 = "switch1";
+    public interface FragmentDeadlineListener {
+        void onInputDeadlineSent(CharSequence input);
 
-    private String text;
-    private Boolean switchedOff;
+    }
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -47,15 +34,6 @@ public class DeadlineFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DeadlineFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static DeadlineFragment newInstance(String param1, String param2) {
         DeadlineFragment fragment = new DeadlineFragment();
         Bundle args = new Bundle();
@@ -72,45 +50,18 @@ public class DeadlineFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
-        textView = (TextView) getView().findViewById(R.id.textViewDate);
-        editText = (EditText) getView().findViewById(R.id.editTextDate);
-        applyButton = (Button) getView().findViewById(R.id.buttonApplyChanges);
-        saveButton = (Button) getView().findViewById(R.id.buttonChange);
-        switch1 = (Switch) getView().findViewById(R.id.switch1);
-
-        applyButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textView.setText(editText.getText().toString());
-            }
-        });
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                saveData();
-            }
-        });
     }
 
-    public void saveData(){
-        SharedPreferences sharedPreferences = this.getActivity().getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-
-        editor.putString(TEXT, textView.getText().toString());
-        editor.putBoolean(SWITCH1, switch1.isChecked());
-    }
-
-    public void loadData(){
-        SharedPreferences sharedPreferences = this.getActivity().getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
-//        Can't be done that way in Fragment:
-//        text = SharedPreferences.getString
-    }
-
+    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_deadline, container, false);
+
+
+        return v;
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_deadline, container, false);
+//        return inflater.inflate(R.layout.fragment_deadline, container, false);
     }
+
 }
